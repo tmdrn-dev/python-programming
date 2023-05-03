5a7200ecac6a55f949e02d616c2ddb4572be2b52
seungku-dev <seungku.dev@gmail.com>
2023-05-04 02:05:11 +0900

    Update 2805.py

--- algorithm/Binary Search/2805.py
+++ algorithm/Binary Search/2805.py
@@ -5,19 +5,20 @@ n, m = map(int, stdin.readline().split())
 tree = sorted(list(map(int, stdin.readline().split())))
 
 from bisect import bisect_left
-h = tree[int(n/2)]
+h = 0
 
-while h < max(tree):
-    l = 0
+while h <= max(tree):
     start = bisect_left(tree, h)
-    for i in tree[start:]:
-        l += i-h
+    l = sum(list(map(lambda x: x-h, tree[start:])))
+    # for i in tree[start:]:
+    #     l += i-h
     if l == m:
         print(h)
         break
-    elif l > m:
-        h += 1
+    elif l < m:
+        print(h-1)
+        break
     else:
-        h -= 1
+        h += 1
 
     # print(start, h, l)
